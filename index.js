d1d8166e5ab73381936a4acc9e9531aeecb66578
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2022-08-17 10:56:53 +0700

    config for vercel

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const express = require('express');
+const app = require('./api/index');
 const mongoose = require('mongoose');
-const cors = require("cors");
-const activityRouter = require('./src/routers/activitiesRoute')
-const scheduleRouter = require('./src/routers/scheduleRoute')
-const config = require('./config')
+const config = require('./config');
 
 
-const app = express();
-
-mongoose.connect(config.mongoDB, {dbName: "VivApplication"})
+const connect = async () => {
+//Connect mongoDB
+await mongoose.connect(config.mongoDB, {dbName: "VivApplication"})
 .then(()=>console.log("Connected Database"))
 .catch((err)=>console.log(err))
 
-
-//middleware
-app.use(cors());
-app.use(express.json())
-
-
-app.use('/activities', activityRouter)
-app.use('/schedule', scheduleRouter)
-
+//start server
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is listen on port: ${config.port}`)
 })
+};
+
+connect();
